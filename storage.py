a1a4b80f5a701c01d2e95950a6ebc850c2db8bfd
MUS2457 <mustaphaamine54@gmail.com>
2025-11-29 12:04:31 +0900

    Delete storage.py

--- storage.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def save_password(password):
-    """Append a password with timestamp to passwords.txt"""
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    entry = f"[{now}] {password}"
-    with open("passwords.txt", "a") as f:
-        f.write(entry + "\n")
